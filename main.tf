2a3f36f448701630c1eef0ea6672438d7815f04d
picccard <eskil.uhlving@gmail.com>
2022-09-15 22:36:12 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
   features {}
 }
 
-
+# remote tfstate
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-euw-az-tf-api-demo"
@@ -23,8 +23,44 @@ terraform {
   }
 }
 
-
-resource "azurerm_resource_group" "rg-euw-tftest" {
+# Reource Group
+resource "azurerm_resource_group" "rg_euw_tftest" {
   name     = "rg-euw-tftest"
   location = "westeurope"
+}
+
+# App Service Plan
+resource "azurerm_app_service_plan" "plan_euw_1" {
+  name                = "plan-euw-win1"
+  location            = azurerm_resource_group.rg_euw_tftest.location
+  resource_group_name = azurerm_resource_group.rg_euw_tftest.name
+  kind                = "Windows"
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+# App Service
+resource "azurerm_app_service" "this" {
+  name                = "app-service-euw-win1-eula"
+  location            = azurerm_resource_group.rg_euw_tftest.location
+  resource_group_name = azurerm_resource_group.rg_euw_tftest.name
+  app_service_plan_id = azurerm_app_service_plan.plan_euw_1.id
+
+  site_config {
+    # Run "az webapp list-runtimes" for current supported values, but always
+    # output the value of process.version from a running app because you might
+    # not get the version you expect
+    windows_fx_version = "node|16-lts"
+  }
+}
+
+# Application Insights
+resource "azurerm_application_insights" "this" {
+  name                = "appi-euw-win1-eula"
+  location            = azurerm_resource_group.rg_euw_tftest.location
+  resource_group_name = azurerm_resource_group.rg_euw_tftest.name
+  application_type    = "web"
 }
