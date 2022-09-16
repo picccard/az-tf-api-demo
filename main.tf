b449d46bf6af9641e1245dfa23ba626c67be57d1
Eskil Larsen <eskil.uhlving@gmail.com>
2022-09-16 03:03:56 +0200

    added US resources

--- main.tf
+++ main.tf
@@ -57,3 +57,38 @@ resource "azurerm_linux_web_app" "webapp" {
     PICCCARD_RANDOM = "custom-env-var-linux1"
   }
 }
+
+# Reource Group US
+resource "azurerm_resource_group" "rg_ue2_tftest" {
+  name     = "rg-ue2-express-api"
+  location = "eastus2"
+}
+
+# App Service Plan US
+resource "azurerm_service_plan" "plan_ue2_1" {
+  name                = "plan-ue2-1"
+  resource_group_name = azurerm_resource_group.rg_ue2_tftest.name
+  location            = azurerm_resource_group.rg_ue2_tftest.location
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+# Webapp US 1
+resource "azurerm_linux_web_app" "webapp-ue2-n1" {
+  name                = "app-service-ue2-linux1-eula"
+  location            = azurerm_resource_group.rg_ue2_tftest.location
+  resource_group_name = azurerm_resource_group.rg_ue2_tftest.name
+  service_plan_id     = azurerm_service_plan.plan_ue2_1.id
+  https_only          = true
+
+  site_config {
+    minimum_tls_version = "1.2"
+    application_stack {
+      node_version = "14-lts"
+    }
+  }
+
+  app_settings = {
+    PICCCARD_RANDOM = "custom-env-var-us2-linux1"
+  }
+}
